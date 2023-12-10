42ece5527688cb236a0379c4702fa22df458cf0c
asif-munshi <asifmunshe2978@gmail.com>
2023-12-10 07:49:49 -0800

    setup shadcnui and nextui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,34 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import './globals.css'
+import { Providers } from './providers'
+import { cn } from '@/lib/utils'
 
 export const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
 
 export const metadata: Metadata = {
-  title: "Epic Games",
-  description: "Created by Asif Munshi",
-};
+  title: 'Epic Games',
+  description: 'Created by Asif Munshi',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          'min-h-screen bg-background font-sans antialiased',
           fontSans.variable
         )}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+//import { Button } from '@/components/ui/button'
+import { Button } from '@nextui-org/button'
+import { Checkbox } from '@nextui-org/checkbox'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Button>Click me</Button>
+      <Checkbox defaultSelected>Option</Checkbox>
     </main>
-  );
+  )
 }
